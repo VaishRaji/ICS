0900ba5a309ac69f8e6e4b2b9014dc84434848c1
Vaishnavi Raji <vaishnavira@infinite.com>
2024-06-15 22:12:16 +0530

    Question one

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -4,12 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp
+namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the first value:");
+            int integerOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second value:");
+            int integerTwo = Convert.ToInt32(Console.ReadLine());
+            if (integerOne == integerTwo)
+            {
+                Console.WriteLine("The numbers are equal");
+
+            }
+            else
+            {
+                Console.WriteLine("they are not equal");
+            }
+            Console.ReadLine();
         }
     }
 }
