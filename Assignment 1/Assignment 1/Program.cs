2d76a5e7c7beeca1160a8013c92deb3ba7ebeacd
Vaishnavi Raji <vaishnavira@infinite.com>
2024-06-16 10:01:27 +0530

    prgm1

---
+++ Assignment 1/Assignment 1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number");
+            int integerOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the number");
+            int integerTwo = Convert.ToInt32(Console.ReadLine());
+            if (integerOne == integerTwo)
+            {
+                Console.WriteLine("They are Equal");
+            }
+            else
+            {
+                Console.WriteLine("They are not Equal");
+            }
+            Console.ReadLine();
+
+        }
+
+    }
+}
